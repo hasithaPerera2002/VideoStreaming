75b0113edde1ed78bab8fa038d2cda11176667e7
hasithaPerera2002 <hasithaperera2017@gmail.com>
2026-07-16 14:21:43 +0530

    implement controller

--- server/Services/DynamodbService.cs
+++ server/Services/DynamodbService.cs
@@ -1,26 +1,68 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 using server.Models;
 
 namespace server.Services;
 
 public class DynamodbService :IVideoMetadataService
 {
-    public Task SaveAsync(VideoMetadata metadata)
+    private readonly ITable _table;
+
+    public DynamodbService(IAmazonDynamoDB client)
     {
-        throw new NotImplementedException();
+        _table = new TableBuilder(client, "VideoMetadata")
+            .AddHashKey("id", DynamoDBEntryType.String)
+            .Build();
     }
 
-    public Task<VideoMetadata?> GetByIdAsync(string id)
+    public async Task SaveAsync(VideoMetadata metadata)
     {
-        throw new NotImplementedException();
+        var doc = new Document
+        {
+            ["id"] = metadata.Id,
+            ["title"] = metadata.Title,
+            ["uploader"] = metadata.Uploader,
+            ["uploadedAt"] = metadata.UploadedAt.ToString("o"),
+            ["s3Key"] = metadata.S3Key,
+            ["thumbnailUrl"] = metadata.ThumbnailUrl ?? "",
+            ["viewCount"] = metadata.ViewCount
+        };
+        await _table.PutItemAsync(doc);
     }
 
-    public Task<List<VideoMetadata>> GetAllAsync()
+    public async Task<VideoMetadata?> GetByIdAsync(string id)
     {
-        throw new NotImplementedException();
+        var doc = await _table.GetItemAsync(id);
+        return doc is null ? null : MapToMetadata(doc);
     }
 
-    public Task DeleteAsync(string id)
+    public async Task<List<VideoMetadata>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var search = _table.Scan(new ScanFilter());
+        var results = new List<VideoMetadata>();
+
+        do
+        {
+            var docs = await search.GetNextSetAsync();
+            results.AddRange(docs.Select(MapToMetadata));
+        } while (!search.IsDone);
+
+        return results;
     }
+
+    public async Task DeleteAsync(string id)
+    {
+        await _table.DeleteItemAsync(id);
+    }
+    
+    private static VideoMetadata MapToMetadata(Document doc) => new()
+    {
+        Id = doc["id"],
+        Title = doc["title"],
+        Uploader = doc["uploader"],
+        UploadedAt = DateTime.Parse(doc["uploadedAt"]),
+        S3Key = doc["s3Key"],
+        ThumbnailUrl = doc["thumbnailUrl"],
+        ViewCount = (long)doc["viewCount"]
+    };
 }
