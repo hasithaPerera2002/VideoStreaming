2225fdb89cbfad1eed57a9eefb882fe868616336
hasithaPerera2002 <hasithaperera2017@gmail.com>
2026-07-16 14:25:39 +0530

    COmplete controller and dynaodb service

--- server/Controllers/VideoController.cs
+++ server/Controllers/VideoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using server.Models;
 using server.Services;
 
 namespace server.Controllers;
@@ -16,4 +17,56 @@ public class VideoController : ControllerBase
         _storageService = storageService;
         _metadataService = metadataService;
     }
+    
+    
+    [HttpPost("upload")]
+    public async Task<IActionResult> Upload(IFormFile file, [FromForm] string title, [FromForm] string uploader)
+    {
+        using var stream = file.OpenReadStream();
+        var s3Key = await _storageService.UploadVideoAsync(stream, file.FileName);
+
+        var metadata = new VideoMetadata
+        {
+            Id = Guid.NewGuid().ToString(),
+            Title = title,
+            Uploader = uploader,
+            S3Key = s3Key
+        };
+
+        await _metadataService.SaveAsync(metadata);
+        return Ok(metadata);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var videos = await _metadataService.GetAllAsync();
+        return Ok(videos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var video = await _metadataService.GetByIdAsync(id);
+        if (video is null) return NotFound();
+        return Ok(video);
+    }
+    
+    [HttpGet("{id}/stream-url")]
+    public async Task<IActionResult> GetStreamUrl(string id)
+    {
+        var video = await _metadataService.GetByIdAsync(id);
+        if (video is null) return NotFound();
+
+        var url = await _storageService.GetVideoUrlAsync(video.S3Key);
+        return Ok(new { url });
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        await _metadataService.DeleteAsync(id);
+        return NoContent();
+    }
+    
 }
